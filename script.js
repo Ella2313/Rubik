ca6c070acf6a7d14c071a8afe25988df6570670e
Ella2313 <joyfullgabi@gmail.com>
2025-10-24 16:10:51 +0300

    Mulan

--- script.js
+++ script.js
@@ -241,4 +241,216 @@ function RoundedBoxGeometry( size, radius, radiusSegments ) {
           }
         }
     }
-    
+    for ( var u = 0; u < radiusSegments; u ++ ) {
+      var a = corneroffset + lastRowOffset + u;
+      var b = corneroffset + lastRowOffset + u + 1;
+      var c = corneroffset + lastVertex;
+      if ( ! flips[ i ] ) {
+        indices.push( a );
+        indices.push( b );
+        indices.push( c );
+      } else {
+        indices.push( a );
+        indices.push( c );
+        indices.push( b );
+      }
+    }
+  }
+}
+function doFaces() {
+  var a = lastVertex;
+  var b = lastVertex + cornerVertNumber;
+  var c = lastVertex + cornerVertNumber * 2;
+  var d = lastVertex + cornerVertNumber * 3;
+  indices.push( a );
+  indices.push( b );
+  indices.push( c );
+  indices.push( a );
+  indices.push( c );
+  indices.push( d );
+  a = lastVertex + cornerVertNumber * 4;
+  b = lastVertex + cornerVertNumber * 5;
+  c = lastVertex + cornerVertNumber * 6;
+  d = lastVertex + cornerVertNumber * 7;
+  indices.push( a );
+  indices.push( c );
+  indices.push( b );
+  indices.push( a );
+  indices.push( d );
+  indices.push( c );
+  a = 0;
+  b = cornerVertNumber;
+  c = cornerVertNumber * 4;
+  d = cornerVertNumber * 5;
+  indices.push( a );
+  indices.push( c );
+  indices.push( b );
+  indices.push( b );
+  indices.push( c );
+  indices.push( d );
+  a = cornerVertNumber * 2;
+    b = cornerVertNumber * 3;
+    c = cornerVertNumber * 6;
+    d = cornerVertNumber * 7;
+    indices.push( a );
+    indices.push( c );
+    indices.push( b );
+    indices.push( b );
+    indices.push( c );
+    indices.push( d );
+    a = radiusSegments;
+    b = radiusSegments + cornerVertNumber * 3;
+    c = radiusSegments + cornerVertNumber * 4;
+    d = radiusSegments + cornerVertNumber * 7;
+    indices.push( a );
+    indices.push( b );
+    indices.push( c );
+    indices.push( b );
+    indices.push( d );
+    indices.push( c );
+    a = radiusSegments + cornerVertNumber;
+    b = radiusSegments + cornerVertNumber * 2;
+    c = radiusSegments + cornerVertNumber * 5;
+    d = radiusSegments + cornerVertNumber * 6;
+    indices.push( a );
+    indices.push( c );
+    indices.push( b );
+    indices.push( b );
+    indices.push( c );
+    indices.push( d );
+  }
+  function doHeightEdges() {
+    for ( var i = 0; i < 4; i ++ ) {
+      var cOffset = i * cornerVertNumber;
+      var cRowOffset = 4 * cornerVertNumber + cOffset;
+      var needsFlip = i & 1 === 1;
+      for ( var u = 0; u < radiusSegments; u ++ ) {
+        var u1 = u + 1;
+        var a = cOffset + u;
+        var b = cOffset + u1;
+        var c = cRowOffset + u;
+        var d = cRowOffset + u1;
+        if ( ! needsFlip ) {
+          indices.push( a );
+          indices.push( b );
+          indices.push( c );
+          indices.push( b );
+          indices.push( d );
+          indices.push( c );
+        } else {
+          indices.push( a );
+          indices.push( c );
+          indices.push( b );
+          indices.push( b );
+          indices.push( c );
+          indices.push( d );
+        }
+      }
+    }
+  }
+  function doDepthEdges() {
+    var cStart = [ 0, 2, 4, 6 ];
+    var CEnd = [ 1, 3, 5, 7 ];
+    for ( var i = 0; i < 4; i ++) {
+      var cStart = cornerVertNumber * cStarts[ i ];
+      var CEnd = cornerVertNumber * CEnds[ i ];
+      var needsFlip = 1 >= i;
+      for ( var u = 0; u < radiusSegments; u ++ ) {
+        var urs1 = u * rs1;
+        var u1rs1 = ( u + 1 ) * rs1;
+        var a = cStart + urs1;
+        var b = cStart + u1rs1;
+        var c = CEnd + urs1;
+        var d = CEnd + u1rs1;
+        if ( needsFlip ) {
+          indices.push( a );
+          indices.push( c );
+          indices.push( b );
+          indices.push( b );
+          indices.push( c );
+          indices.push( d );
+        } else {
+          indices.push( a );
+          indices.push( b );
+          indices.push( c );
+          indices.push( b );
+          indices.push( d );
+          indices.push( c );
+        }
+      }
+    }
+  }
+  function doWidthEdges() {
+    var end = radiusSegments - 1;
+    var cStarts = [ 0, 1, 4, 5 ];
+    var cEnds = [ 3, 2, 7, 6 ];
+    var needsFlip = [ 0, 1, 1, 0 ];
+    for ( var i = 0; i < 4; i ++ ) {
+      var cStart = cStarts[ i ] * cornerVertNumber;
+      var cEnd = cEnds[ i ] * cornerVertNumber;
+      for ( var u = 0; u <= end; u ++ ) {
+        var a = cStart + radiusSegments + u * rs1;
+        var b = cStart + ( u != end ? radiusSegments + ( u + 1 ) * rs1 : cornerVertNumber - 1 );
+        var c = cEnd + radiusSegments + u * rs1;
+        var d = cEnd + ( u != end ? radiusSegments + ( u + 1 ) * rs1 : cornerVertNumber - 1 );
+        if ( ! needsFlip[ i ] ) {
+          indices.push( a );
+          indices.push( b );
+          indices.push( c );
+          indices.push( b );
+          indices.push( d );
+          indices.push( c );
+        } else {
+          indices.push( a );
+          indices.push( c );
+          indices.push( b );
+          indices.push( b );
+          indices.push( c );
+          indices.push( d );
+        }
+      }
+    }
+  }
+  var index = 0;
+  for ( var i = 0; i < vertexPool.length; i ++ ) {
+    positions.setXYZ(
+      index,
+      vertexPool[ i ].x,
+      vertexPool[ i ].y,
+      vertexPool[ i ].z
+    );
+    normals.setXYZ(
+      index,
+      normalPool[ i ].x,
+      normalPool[ i ].y,
+      normalPool[ i ].z
+    );
+    index ++;
+  }
+  this.setIndex( new THREE.BufferAttribute( new Uint16Array( indices ), 1 ) );
+  this.addAttribute( 'position', positions );
+  this.addAttribute( 'normal', normals );
+}
+RoundedBoxGeometry.prototype = Object.create( THREE.BufferGeometry.prototype );
+RoundedBoxGeometry.constructor = RoundedBoxGeometry;
+function RoundedPlaneGeometry( size, radius, depth ) {
+  var x, y, width, height;
+  x = y = - size / 2;
+  width = height = size;
+  radius = size * radius;
+  const shape = new THREE.Shape();
+  shape.moveTo( x, y + radius );
+  shape.lineTo( x, y + height - radius );
+  shape.quadraticCurveTo( x, y + height, x + radius, y + height );
+  shape.lineTo( x + width - radius, y + height );
+  shape.quadraticCurveTo( x + width, y + height, x + width, y + height - radius );
+  shape.lineTo( x + width, y + radius );
+  shape.quadraticCurveTo( x + width, y, x + width - radius, y );
+  shape.lineTo( x + radius, y );
+  shape.quadraticCurveTo( x, y, x, y + radius );
+  const geometry = new THREE.ExtrudeBufferGeometry(
+    shape,
+    { depth: depth, bevelEnabled: false, curveSegments: 3 }
+  );
+  return geometry;
+}
